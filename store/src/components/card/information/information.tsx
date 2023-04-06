30330f860d28e1e6792f5c21f3e96b87bcb8fa9a
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2023-04-06 18:48:14 +0200

    feat: create component information

--- store/src/components/card/information/information.tsx
+++ store/src/components/card/information/information.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import  React from "react";
 import { PRODUCTS } from "../../../constants/constants";
 
 export const Information = () => {
   const information: [{stock: number}, {type: string}, { rating: number}, {price: number}] = PRODUCTS[0].info;
   return (
     <div>
-        <p>{information[0].stock}</p>
+      { information?.map((item): React.ReactNode => {
+        const item2 = Object.entries(item).flat();
+          return  <li>{`${item2[0]}: ${item2[1]}`}</li>
+        }) 
+      }
     </div>
   )
 }
