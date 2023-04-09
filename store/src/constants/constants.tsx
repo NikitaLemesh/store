aa1f0a5b8ee536f5d8b8c8425792c06045b9ca8e
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2023-04-09 14:25:40 +0200

    fix: change constants

--- store/src/constants/constants.tsx
+++ store/src/constants/constants.tsx
@@ -2,59 +2,51 @@ import { Item } from "./interface"
 
 export const PRODUCTS: Item[] = [
   {
-    id: 1,
+    id: 0,
     brand: 'nike',
     description: 'Wearallday CJ1682 004 Black/White',
     imageFirst: './images/nike1.jpg',
     imageSecond: './images/nike1.1.jpg',
     imageThird: './images/nike1.2.jpg',
-    info: [
-      { stock: 62 },
-      { type: 'shoes' },
-      { rating:  5.5 },
-      { price: 60 }
-    ]
+    stock: 62 ,
+    type: 'shoes',
+    rating:  5.5,
+    price: 60
   },
   {
-    id: 2,
+    id: 1,
     brand: 'nike',
     description: 'Court Vision Lo Nn DH2987 106 White/Midnight Navy',
     imageFirst: './images/nike2.jpg',
     imageSecond: './images/nike2.1.jpg',
     imageThird: './images/nike2.2.jpg',
-    info: [
-      { stock: 31 },
-      { type: 'shoes' },
-      { rating:  6.5 },
-      { price: 81 }
-    ]
+    stock: 31,
+    type: 'shoes',
+    rating:  6.5,
+    price: 81
   },
   {
-    id: 3,
+    id: 2,
     brand: 'new balance',
     description: 'NEW BALANCE SNEAKERS ML574EAG Grey',
     imageFirst: './images/nb1.jpg',
     imageSecond: './images/nb1.1.jpg',
     imageThird: './images/nb1.2.jpg',
-    info: [
-      { stock: 46 },
-      { type: 'shoes' },
-      { rating:  9.5 },
-      { price: 95 }
-    ]
+    stock: 46,
+    type: 'shoes',
+    rating:  9.5,
+    price: 95
   },
   {
-    id: 4,
+    id: 3,
     brand: 'new balance',
     description: 'NEW BALANCE SNEAKERS GM500BUS Burgundy',
     imageFirst: './images/nb2.jpg',
     imageSecond: './images/nb2.1.jpg',
     imageThird: './images/nb2.2.jpg',
-    info: [
-      { stock: 28 },
-      { type: 'shoes' },
-      { rating:  9 },
-      { price: 89 }
-    ]
+    stock: 28,
+    type: 'shoes',
+    rating:  9,
+    price: 89
   }
 ]
